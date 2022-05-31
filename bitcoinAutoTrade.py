b3671a40d4484769147173380960cefb1501b5ca
Yi <hoon_5083@naver.com>
2022-06-01 01:43:37 +0900

    <edit> coin type, strategy

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -54,7 +54,6 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
-
 # 자동매매 시작
 while True:
     try:
@@ -65,16 +64,15 @@ while True:
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price(cryptoRelation, 0.5)
-            ma15 = get_ma15(cryptoRelation)
             current_price = get_current_price(cryptoRelation)
-            if target_price < current_price and ma15 < current_price:
-                krw = get_balance(cryptoType)
+            if target_price < current_price:
+                krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order(cryptoRelation, krw*0.9995)
         else:
-            btc = get_balance(cryptoType)
-            if btc > 0.00008:
-                upbit.sell_market_order(cryptoRelation, btc)
+            cryptoBalance = get_balance(cryptoType)
+            if cryptoBalance > (5000/get_current_price(cryptoRelation)):
+                upbit.sell_market_order(cryptoRelation, cryptoBalance)
         time.sleep(1)
     except Exception as e:
         print(e)
